baf609cf7e05f5ed3d2b5abf52ee64623ef4e625
AlCher38 <154320556+AlCher38@users.noreply.github.com>
2024-03-06 08:51:14 +0300

    Update user.py

--- src/user.py
+++ src/user.py
@@ -1,29 +1,17 @@
-def interact_with_user():
-    while True:
-        print("1. Ввести поисковый запрос для запроса вакансий из hh.ru")
-        print("2. Получить топ N вакансий по зарплате")
-        print("3. Получить вакансии с ключевым словом в описании")
-        print("4. Выход")
+from src.vacancy import JobSearchApp
 
-        choice = input("Выберите действие (1/2/3/4): ")
 
-        if choice == "1":
-            search_query = input("Введите поисковый запрос для запроса вакансий из hh.ru: ")
-            # Implement logic to request vacancies from hh.ru based on search_query
-            # Display the retrieved vacancies to the user
-            print("Вакансии, найденные по запросу:", search_query)
-        elif choice == "2":
-            top_n = int(input("Введите количество вакансий для топ N по зарплате: "))
-            # Implement logic to retrieve top N vacancies by salary
-            # Display the top N vacancies to the user
-            print(f"Топ {top_n} вакансий по зарплате:")
-        elif choice == "3":
-            keyword = input("Введите ключевое слово для поиска в описании вакансий: ")
-            # Implement logic to retrieve vacancies with the specified keyword in the description
-            # Display the retrieved vacancies to the user
-            print("Вакансии с ключевым словом в описании:", keyword)
-        elif choice == "4":
-            print("До свидания!")
-            break
-        else:
-            print("Неверный выбор. Пожалуйста, выберите действие 1, 2, 3 или 4.")
+def user_interaction():
+    """
+     Запрашивает у пользователя ключ API, имя файла JSON, поисковый запрос, верхнее значение N и ключевое
+    слово для взаимодействия с JobSearchApp и выполнения различных операций поиска вакансий.
+     """
+    api_key = input("Введите API ключ для hh.ru: ")
+    json_filename = input("Введите имя файла для сохранения вакансий в формате JSON: ")
+    job_app = JobSearchApp(api_key, json_filename)
+    search_query = input("Введите поисковый запрос для запроса вакансий из hh.ru: ")
+    job_app.search_and_save_vacancies(search_query)
+    top_n = int(input("Введите количество вакансий для вывода топ N по зарплате: "))
+    job_app.get_top_n_vacancies(top_n)
+    keyword = input("Введите ключевое слово для поиска вакансий по описанию: ")
+    job_app.search_vacancies_by_keyword(keyword)
